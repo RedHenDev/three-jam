e5c669da8d7ed3dde682cec33eaf929e6373ec08
RedHenDev <neobn@hotmail.com>
2025-05-22 18:09:29 +0100

    Revert "terrain perlin fail"
    This reverts commit ecea374bc7a5392e5f5f0bc1c056f5fa77a4cc98.

--- js/terrain.js
+++ js/terrain.js
@@ -2,16 +2,12 @@ let plane;
 
 function setupTerrain() {
     const planeGeometry = new THREE.PlaneGeometry(50, 50);
-    const planeMaterial = new THREE.MeshStandardMaterial({ 
-        color: 0x208020, 
-        side: THREE.DoubleSide 
-    });
+    const planeMaterial = new THREE.MeshStandardMaterial({ color: 0x208020, side: THREE.DoubleSide });
     plane = new THREE.Mesh(planeGeometry, planeMaterial);
-    plane.rotation.x = Math.PI / 2;  // This aligns with our top-down view
+    plane.rotation.x = Math.PI / 2;
     plane.receiveShadow = true;
     scene.add(plane);
 
     const gridHelper = new THREE.GridHelper(50, 50);
-    gridHelper.position.y = -0.01; // Slightly below plane to avoid z-fighting
     scene.add(gridHelper);
 }
