851ae490c17cb174d52800642aee81171334651e
RedHenDev <neobn@hotmail.com>
2025-05-22 18:09:46 +0100

    Revert "updated GROUND_OFFSET"
    This reverts commit 1aab04d294050d3f18e9ebac1bf1254995b6498b.

--- js/movement.js
+++ js/movement.js
@@ -3,14 +3,13 @@ let moveVelX = 0;
 let moveVelZ = 0;
 
 const GRAVITY = 0.004;
-const THRUST_FORCE = 0.006; // 0.008
+const THRUST_FORCE = 0.008;
 const MAX_VELOCITY = 0.2;
 const MOVE_ACCEL = 0.002;
 const MOVE_DECEL = 0.001;
 const MAX_MOVE_SPEED = 0.2;
-const MAX_HEIGHT = 4;
+const MAX_HEIGHT = 2;
 const GROUND_FRICTION = 0.15;
-const GROUND_OFFSET = 0.4;
 
 function updateMovement() {
     updateVerticalMovement();
@@ -23,15 +22,15 @@ function updateVerticalMovement() {
     }
     velocity -= GRAVITY;
     velocity = Math.min(Math.max(velocity, -MAX_VELOCITY), MAX_VELOCITY);
-    cube.position.y = Math.min(Math.max(cube.position.y + velocity, GROUND_OFFSET), MAX_HEIGHT);
+    cube.position.y = Math.min(Math.max(cube.position.y + velocity, 1), MAX_HEIGHT);
 
-    if (cube.position.y <= GROUND_OFFSET) {
+    if (cube.position.y <= 1) {
         velocity = 0;
     }
 }
 
 function updateHorizontalMovement() {
-    if (cube.position.y > GROUND_OFFSET) {
+    if (cube.position.y > 1) {
         applyAirControls();
     } else {
         applyGroundFriction();
